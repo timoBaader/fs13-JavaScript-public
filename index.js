ec9f589c6527426d08e2c01969e791a6543c887a
Timo Baader <Timo.Baader91@gmail.com>
2022-12-12 18:39:07 +0200

    second exercise done

--- index.js
+++ index.js
@@ -32,14 +32,31 @@ let myArr = [
   "2022-14-12",
 ];
 const fixDate = (array) => {
-  /* provide your code here */
+  let result = array.map((item) => {
+    // check for first - to know the format
+    let index = item.indexOf("-", 3);
+    // index == 7 means the year is in the middle
+    if (index == 7) {
+      item = item.slice(0, 2) + item.slice(7) + "-" + item.slice(3, 7);
+    }
+    // index == 4 means the year is at the start
+    else if (index == 4) {
+      item = item.slice(5) + "-" + item.slice(0, 4);
+    }
+    // if the month is bigger than 12, it is supposed to be the day instead
+    if (item.slice(3, 5) > 12) {
+      item = item.slice(3, 5) + "-" + item.slice(0, 2) + item.slice(5);
+    }
+    return item;
+  });
+  return result;
 };
 let newArr = fixDate(myArr);
 console.log(newArr);
 
 /*
 3. Counter function
-Write a counter funtion to print out in console the time difference between 2 given date
+Write a counter function to print out in console the time difference between 2 given date
 Expected result in the console: 11 days - 13 hours - 38 minutes - 20 seconds
 */
 const dateFrom = new Date(500000);
--- test.js
+++ test.js
@@ -1,7 +1,32 @@
-const printNum = () => {
-  for (let i = 0; i <= 100; i++) {
-    setTimeout(() => console.log(i), 1000);
-  }
-};
+let myArr = [
+  "12-24-2014",
+  "09-2022-23",
+  "12-30-2021",
+  "08-02-2021",
+  "07-15-2018",
+  "2019-12-14",
+  "2022-14-12",
+];
 
-printNum();
+const fixDate = (array) => {
+  let result = array.map((item) => {
+    // check for first - to know the format
+    let index = item.indexOf("-", 3);
+    // index == 7 means the year is in the middle
+    if (index == 7) {
+      item = item.slice(0, 2) + item.slice(7) + "-" + item.slice(3, 7);
+    }
+    // index == 4 means the year is at the start
+    else if (index == 4) {
+      item = item.slice(5) + "-" + item.slice(0, 4);
+    }
+    // if the month is bigger than 12, it is supposed to be the day instead
+    if (item.slice(3, 5) > 12) {
+      item = item.slice(3, 5) + "-" + item.slice(0, 2) + item.slice(5);
+    }
+    return item;
+  });
+  return result;
+};
+let newArr = fixDate(myArr);
+console.log(newArr);
